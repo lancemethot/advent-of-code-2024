c7f343812c1170346d96745ced9ee633393f9822
lancemethot <132783723+lancemethot@users.noreply.github.com>
2024-12-02 01:29:25 -0500

    day 2 part 2

--- day02/day02.test.ts
+++ day02/day02.test.ts
@@ -17,31 +17,58 @@ function parseInput(input: string[]): Report [] {
     return reports;
 }
 
-function isSafe(report: Report): boolean {
-    let direction: 'ASC' | 'DESC' = 'ASC';
-    for(let i = 0; i < report.levels.length - 1; i++) {
-        const curr = report.levels[i];
-        const next = report.levels[i + 1];
-        const distance = Math.abs(curr - next);
-        if(distance < 0 || distance > 3) {
-            return false;
-        }
-        if(i === 0) {
-            direction = next < curr ? 'DESC' : 'ASC';
-        }
-        if(direction === 'ASC' && next <= curr) return false;
-        if(direction === 'DESC' && next >= curr) return false;
+function isAllIncreasing(levels: number[]): boolean {
+    for(let i = 0; i < levels.length - 1; i++) {
+        if(levels[i] >= levels[i + 1]) return false;
     }
     return true;
 }
 
+function isAllDecreasing(levels: number[]): boolean {
+    for(let i = 0; i < levels.length - 1; i++) {
+        if(levels[i] <= levels[i + 1]) return false;
+    }
+    return true;
+}
+
+function isAllWithinRange(levels: number[]): boolean {
+    for(let i = 0; i < levels.length - 1; i++) {
+        const distance = Math.abs(levels[i] - levels[i + 1]);
+        if(distance < 1 || distance > 3) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function isSafe(levels: number[]): boolean {
+    return isAllWithinRange(levels) && (isAllDecreasing(levels) || isAllIncreasing(levels));
+}
+
+function anySafe(levels: number[]): boolean {
+    for(let i = 0; i < levels.length; i++) {
+        const filtered = levels.filter((v, idx) => i !== idx);
+        if(isSafe(filtered)) return true;
+    }
+    return false;
+}
+
 function partOne(input: string[]): number {
     return parseInput(input).reduce((acc, report) => {
-        return acc += isSafe(report) ? 1 : 0;
+        return acc += isSafe(report.levels) ? 1 : 0;
+    }, 0);
+}
+
+function partTwo(input: string[]): number {
+    return parseInput(input).reduce((acc, report) => {
+        return acc += isSafe(report.levels) || anySafe(report.levels) ? 1 : 0;
     }, 0);
 }
 
 test(day, () => {
     expect(partOne(getExampleInput(day))).toBe(2);
     expect(partOne(getDayInput(day))).toBe(606);
+
+    expect(partTwo(getExampleInput(day))).toBe(4);
+    expect(partTwo(getDayInput(day))).toBe(644);
 });
