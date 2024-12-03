361cca28cc507fb651bf78b05264a0bfc02dd205
lancemethot <132783723+lancemethot@users.noreply.github.com>
2024-12-03 00:32:38 -0500

    day 3 part 2

--- day03/day03.test.ts
+++ day03/day03.test.ts
@@ -6,26 +6,60 @@ type Instruction = {
     operands: number [];
 }
 
-function parseInput(input: string[]): Instruction[] {
-    return input.reduce((acc, val) => {
-        const match = val.match(/mul\(\d+,\d+\)/g);
-        match?.forEach(m => {
-            const operands = m.match(/\d+/g);
-            acc.push({
-                operands: operands?.map(o => Number.parseInt(o)) || []
-            })
-        });
-        return acc;
-    }, [] as Instruction[]);
+function findInstructions(input: string): Instruction[] {
+    const instructions: Instruction[] = [];
+    const match = input.match(/mul\(\d+,\d+\)/g);
+    match?.forEach(m => {
+        const operands = m.match(/\d+/g);
+        instructions.push({
+            operands: operands?.map(o => Number.parseInt(o)) || []
+        })
+    });
+    return instructions;
 }
 
 function partOne(input: string[]): number {
-    return parseInput(input).reduce((acc, val) => {
+    return findInstructions(input.join('')).reduce((acc, val) => {
         return acc += val.operands[0] * val.operands[1];
     }, 0);
+}
+
+// if enabled, find up to the next don't()
+// if disabled, find up to the next do()
+// only add when enabled, ignore when disabled
+
+
+function partTwo(input: string[]): number {
+    let check = input.join('');
+    let index = 0;
+    let isEnabled = true;
+    let nextDont = check.length;
+    let total = 0;
+
+    while(true) {
+        if(isEnabled) {
+            index = check.indexOf('don\'t()');
+            total = findInstructions(check.substring(0, index < 0 ? check.length : index)).reduce((acc, val) => {
+                return acc += val.operands[0] * val.operands[1];
+            }, total);
+            isEnabled = false;
+            if(index < 0) break;
+            check = check.substring(index);
+        } else {
+            index = check.indexOf('do()');
+            if(index < 0) break;
+            isEnabled = true;
+            check = check.substring(index);
+        }
+    }
+
+    return total;
 }
 
 test(day, () => {
     expect(partOne(getExampleInput(day))).toBe(161);
     expect(partOne(getDayInput(day))).toBe(189527826);
+
+    expect(partTwo(getExampleInput(day, 2))).toBe(48);
+    expect(partTwo(getDayInput(day))).toBe(63013756);
 });
