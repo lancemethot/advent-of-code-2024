aad00f6732dd5bbb2629440f65c4a5f259eeb317
lancemethot <132783723+lancemethot@users.noreply.github.com>
2024-12-05 07:53:46 -0500

    day 5 part 2

--- day05/day05.test.ts
+++ day05/day05.test.ts
@@ -41,11 +41,17 @@ function parseInput(input: string[]): { rules: Rule[], updates: Update[] } {
     }
 }
 
-function isInRightOrder(rule: Rule, update: Update): boolean {
+function doesRuleApply(rule: Rule, update: Update): boolean {
     let firstIndex = update.pageNumbers.indexOf(rule.first);
     let secondIndex = update.pageNumbers.indexOf(rule.second);
-    if(firstIndex !== -1 && secondIndex !== -1) {
-        return firstIndex < secondIndex;
+    return firstIndex !== -1 && secondIndex !== -1;
+}
+
+function isInRightOrder(rule: Rule, update: Update): boolean {
+    if(doesRuleApply(rule, update)) {
+        let firstIndex = update.pageNumbers.indexOf(rule.first);
+        let secondIndex = update.pageNumbers.indexOf(rule.second);
+        return firstIndex < secondIndex; 
     }
     return true;
 }
@@ -54,6 +60,31 @@ function isAllInRightOrder(rules: Rule[], update: Update): boolean {
     return rules.reduce((acc, rule) => {
         return acc && isInRightOrder(rule, update);
     }, true);
+}
+
+function reorderUpdate(rules: Rule[], update: Update): Update {
+    const pageNumbers = update.pageNumbers.map(n => n);
+    let done = false;
+    while(!done) {
+        let ruleIndex = 0;
+        for(ruleIndex = 0; ruleIndex < rules.length; ruleIndex++) {
+            const rule = rules[ruleIndex];
+            let applies = doesRuleApply(rule, update);
+            let ordered = isInRightOrder(rule, { pageNumbers: pageNumbers });
+            if(applies && !ordered) {
+                let firstIndex = pageNumbers.indexOf(rule.first);
+                let secondIndex = pageNumbers.indexOf(rule.second);
+                pageNumbers.splice(firstIndex, 1, rule.second);
+                pageNumbers.splice(secondIndex, 1, rule.first);
+                break;
+            }
+        }
+        done = ruleIndex === rules.length;
+    }
+
+    return {
+        pageNumbers: pageNumbers
+    };
 }
 
 function getMiddlePart(update: Update): number {
@@ -67,7 +98,19 @@ function partOne(input: string[]): number {
     }, 0);
 }
 
+function partTwo(input: string[]): number {
+    const { rules, updates } = parseInput(input);
+    return updates.filter(update => !isAllInRightOrder(rules, update))
+        .map(update => reorderUpdate(rules, update))
+        .reduce((acc, update) => {
+            return acc += getMiddlePart(update);
+        }, 0);
+}
+
 test(day, () => {
     expect(partOne(getExampleInput(day))).toBe(143);
-    expect(partOne(getDayInput(day))).toBe(0);
+    expect(partOne(getDayInput(day))).toBe(3608);
+
+    expect(partTwo(getExampleInput(day))).toBe(123);
+    expect(partTwo(getDayInput(day))).toBe(4922);
 });
