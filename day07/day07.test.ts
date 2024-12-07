5f4d4c193802dcd5eac93e27c6582abd927818b0
lancemethot <132783723+lancemethot@users.noreply.github.com>
2024-12-07 06:49:02 -0500

    day 7 part 2

--- day07/day07.test.ts
+++ day07/day07.test.ts
@@ -46,6 +46,35 @@ function isSolvable(equation: Equation): boolean {
     }).length > 0;
 }
 
+function isSolvableWithConcat(equation: Equation): boolean {
+    // create a list of combinations to check
+    let check: string[] = ['+', '*', '|'];
+    for(let i = 2; i < equation.numbers.length; i++) {
+        let additional: string[] = check.reduce((acc, c) => {
+            acc.push(`${c}+`,`${c}*`, `${c}|`);
+            return acc;
+        }, [] as string[]);
+        // replace checklist
+        check = additional;
+    }
+
+    // attempt each combo
+    return check.filter(combination => {
+        let operators = combination.split('');
+        let sumOrProduct = equation.numbers[0];
+        for(let i = 0; i < operators.length; i++) {
+            if(operators[i] === '+') {
+                sumOrProduct += equation.numbers[i + 1];
+            } else if(operators[i] === '*') {
+                sumOrProduct *= equation.numbers[i + 1]; 
+            } else {
+                sumOrProduct = Number.parseInt(`${sumOrProduct}${equation.numbers[i + 1]}`);
+            }
+        }
+        return sumOrProduct === equation.expect;
+    }).length > 0;
+}
+
 function partOne(input: string[]): number {
     return parseInput(input)
         .filter(equation => isSolvable(equation))
@@ -54,7 +83,18 @@ function partOne(input: string[]): number {
         }, 0);
 }
 
+function partTwo(input: string[]): number {
+    return parseInput(input)
+        .filter(equation => isSolvableWithConcat(equation))
+        .reduce((acc, equation) => {
+            return acc += equation.expect;
+        }, 0);
+}
+
 test(day, () => {
     expect(partOne(getExampleInput(day))).toBe(3749);
-    expect(partOne(getDayInput(day))).toBe(0);
+    expect(partOne(getDayInput(day))).toBe(7579994664753);
+
+    expect(partTwo(getExampleInput(day))).toBe(11387);
+    expect(partTwo(getDayInput(day))).toBe(438027111276610);
 });
