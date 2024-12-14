d443c1b0d746004b136a5c57a58879055ae44d0c
lancemethot <132783723+lancemethot@users.noreply.github.com>
2024-12-13 22:18:44 -0500

    day 13 part 2

--- day13/day13.test.ts
+++ day13/day13.test.ts
@@ -1,4 +1,4 @@
-import { debug, getExampleInput, getDayInput } from '../utils';
+import { getExampleInput, getDayInput } from '../utils';
 
 const day = 'day13';
 
@@ -11,11 +11,6 @@ type ClawMachine = {
     a: Coord;
     b: Coord;
     prize: Coord;
-}
-
-type Game = {
-    buttonAPresses: number;
-    buttonBPresses: number;
 }
 
 function parseCoords(line: string): Coord {
@@ -39,128 +34,42 @@ function parseInput(input: string[]): ClawMachine[] {
     }, [] as ClawMachine[]);
 }
 
-function slope(from: Coord, to: Coord): number {
-    const rise: number = to.y - from.y;
-    const run: number = to.x - from.x;
-    return rise / run;
-}
-
-function distance(from: Coord, to: Coord): number {
-    const rise: number = to.y - from.y;
-    const run: number = to.x - from.x;
-    return Math.sqrt((rise * rise) + (run * run))
-}
-
-function getCoordsOnLine(from: Coord, to: Coord, times: number): Coord [] {
-    const rise: number = to.y - from.y;
-    const run: number = to.x - from.x;
-    const slope: number = rise / run;
-    const angle = Math.atan(slope);
-    const coordsOnLine: Coord[] = [ from, to ];
-    let distance = Math.sqrt((rise * rise) + (run * run));
-    let offset = 0;
-
-    while(times-- > 0) {
-        // increase distance by 1 until out of bounds
-        offset += distance;
-
-        const distanceX = offset * Math.cos(angle);
-        const distanceY = offset * Math.sin(angle);
-
-        coordsOnLine.push({
-            x: Math.round(to.x + distanceX) + 0,
-            y: Math.round(to.y + distanceY) + 0
-        });
+function calculateFewestTokens(machine: ClawMachine): number {
+    // Solve with linear algebra
+    // Get wholly-divisible button presses for a and b
+    let ap = ((machine.prize.x * machine.b.y) - (machine.prize.y * machine.b.x)) / ((machine.a.x * machine.b.y) - (machine.a.y * machine.b.x));
+    let bp = ((machine.prize.y * machine.a.x) - (machine.prize.x * machine.a.y)) / ((machine.a.x * machine.b.y) - (machine.a.y * machine.b.x));
+    if(Number.isInteger(ap) && Number.isInteger(bp)) {
+        // If evenly divisble, return cost
+        return (ap * 3) + bp;
     }
-
-    return coordsOnLine;
-}
-
-function determinePresses(firstButton: Coord, secondButton: Coord, prize: Coord, maxPresses: number): [ number, number ] {
-    let firstPresses = 0;
-    let secondPresses = 0;
-
-    let firstButtonLine: Coord[] = getCoordsOnLine({ x: 0, y: 0 }, firstButton, maxPresses)
-        .filter(coord => coord.x <= prize.x && coord.y <= prize.y );
-
-    if(firstButtonLine.filter(coord => coord.x === prize.x && coord.y === prize.y).length > 0) {
-        debug(`First button winner: ${firstButtonLine.indexOf(prize)}`, day);
-        return [ firstButtonLine.length, 0 ];
-    }
-
-    while(firstButtonLine.length > 0) {
-        let check = firstButtonLine.pop() as Coord;
-        let remainingPresses = maxPresses - firstButtonLine.length + 1;
-        let secondButtonLine: Coord[] = getCoordsOnLine(check, { x: check.x + secondButton.x, y: check.y + secondButton.y }, remainingPresses)
-            .filter(coord => coord.x <= prize.x && coord.y <= prize.y);
-        debug(`First Presses: ${firstButtonLine.length + 1}\tSecond Presses: ${secondButtonLine.length}\t:Last Coord: ${secondButtonLine[secondButtonLine.length-1].x},${secondButtonLine[secondButtonLine.length-1].y}`, day);
-        if(secondButtonLine.filter(coord => coord.x === prize.x && coord.y === prize.y).length > 0) {
-            debug(`Second button winner: ${secondButtonLine.indexOf(prize)}`, day);
-            return [ firstButtonLine.length + 1, secondButtonLine.length ];
-        }
-    }
-
-    return [ firstPresses, secondPresses ];
-}
-
-function playGame(machine: ClawMachine, maxPresses: number): Game {
-    const aDistance = distance({ x: 0, y: 0 }, machine.a);
-    const bDistance = distance({ x: 0, y: 0 }, machine.b);
-
-    determinePresses(machine.a, machine.b, machine.prize, maxPresses);
-    determinePresses(machine.b, machine.a, machine.prize, maxPresses);
-    
-    let presses: [number, number] = [0,0];
-    if((aDistance / 3) > bDistance) {
-        // Button A gives more distance per token
-        presses = determinePresses(machine.a, machine.b, machine.prize, maxPresses);
-        return {
-            buttonAPresses: presses[0],
-            buttonBPresses: presses[1]
-        };
-    } else {
-        // Start with Button B
-        presses = determinePresses(machine.b, machine.a, machine.prize, maxPresses);
-        return {
-            buttonAPresses: presses[1],
-            buttonBPresses: presses[0]
-        };
-    }
-}
-
-function findWinners(machine: ClawMachine, maxPresses: number): Game[] {
-    let winners: Game[] = [];
-
-    for(let ap = 0; ap < maxPresses; ap++) {
-        for(let bp = 0; bp < maxPresses; bp++) {
-            if((((machine.a.x * ap) + (machine.b.x * bp)) === machine.prize.x) &&
-               (((machine.a.y * ap) + (machine.b.y * bp)) === machine.prize.y)) {
-                winners.push({
-                    buttonAPresses: ap,
-                    buttonBPresses: bp
-                })
-            }
-        }
-    }
-    return winners;
-}
-
-function calculateFewestTokens(machine: ClawMachine, maxPresses: number): number {
-    const winners: Game[] = findWinners(machine, maxPresses);
-    if(winners.length === 0) return 0;
-    return winners.reduce((acc, winner) => {
-        return Math.min(acc, ((winner.buttonAPresses * 3) + winner.buttonBPresses));
-    }, Number.MAX_VALUE);
+    // Not winnable, don't play
+    return 0;
 }
 
 function partOne(input: string[]): number {
     return parseInput(input).reduce((acc, machine) => {
-        return acc += calculateFewestTokens(machine, 100);
+        return acc += calculateFewestTokens(machine);
+    }, 0);
+}
+
+function partTwo(input: string[]): number {
+    return parseInput(input).reduce((acc, machine) => {
+        return acc += calculateFewestTokens({
+            a: machine.a,
+            b: machine.b,
+            prize: {
+                x: machine.prize.x + 10000000000000,
+                y: machine.prize.y + 10000000000000,
+            }
+        });
     }, 0);
 }
 
 test(day, () => {
-    debug(`Date: ${new Date()}\n`, day, false);
     expect(partOne(getExampleInput(day))).toBe(480);
     expect(partOne(getDayInput(day))).toBe(26299);
+
+    expect(partTwo(getExampleInput(day))).toBe(875318608908);
+    expect(partTwo(getDayInput(day))).toBe(107824497933339);
 })
