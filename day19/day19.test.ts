f7a37d2847ab006e80aeb3dfe17b3793877d39bb
lancemethot <132783723+lancemethot@users.noreply.github.com>
2024-12-19 18:05:55 -0500

    day 19 code cleanup

--- day19/day19.test.ts
+++ day19/day19.test.ts
@@ -1,11 +1,6 @@
-import { debug, getDayInput, getExampleInput } from '../utils';
+import { getDayInput, getExampleInput } from '../utils';
 
 const day = 'day19';
-
-type Combination = {
-    index: number;
-    design: string[];
-}
 
 function parseInput(input: string[]): { patterns: Set<string>, designs: string[] } {
     return input.reduce((acc, line) => {
@@ -14,31 +9,6 @@ function parseInput(input: string[]): { patterns: Set<string>, designs: string[]
         return acc;
     }, { patterns: new Set<string>(), designs: [] as string[] });
 }
-
-/*
-def possible_arrangements(towels, design, max_towel_length, memo):
-    if design in memo:        
-        return memo[design]    
-    if len(design) == 0:
-        return 0
-    for i in range(1, min(len(design), max_towel_length) + 1):
-        current = design[:i]
-        if current in towels:
-            if len(design) == i:
-                update_memo(memo, design, 1)
-            else:
-                rhs_arrangements = possible_arrangements(towels, design[i:], max_towel_length, memo)
-                update_memo(memo, design, rhs_arrangements)
-    if design not in memo:
-        memo[design] = 0
-    return memo[design]
-
-def update_memo(memo, design, arrangements):
-    if design in memo:
-        memo[design] += arrangements
-    else:
-        memo[design] = arrangements
-*/
 
 function updateMemo(memo: Map<string, number>, design: string, arrangements: number): Map<string, number> {
     if(memo.has(design)) {
@@ -50,15 +20,21 @@ function updateMemo(memo: Map<string, number>, design: string, arrangements: num
 }
 
 function arrangements(patterns: Set<string>, design: string, max: number, memo: Map<string, number>): number {
+    // already calculated
     if(memo.has(design)) return memo.get(design) as number;
+    // nothing to check
     if(design.length === 0) return 0;
+    // which string is longer, design or max pattern?
     let limit = Math.min(design.length, max) + 1;
     for(let i = 0; i < limit; i++) {
-        let current = design.substring(0, i);
-        if(patterns.has(current)) {
+        // check substring
+        let check = design.substring(0, i);
+        if(patterns.has(check)) {
             if(design.length === i) {
+                // increment design match
                 memo = updateMemo(memo, design, 1);
             } else {
+                // check again with substring
                 let possible = arrangements(patterns, design.substring(i), max, memo);
                 memo = updateMemo(memo, design, possible);
             }
@@ -70,28 +46,13 @@ function arrangements(patterns: Set<string>, design: string, max: number, memo: 
     return memo.get(design) as number;
 }
 
-function combinations(patterns: Set<string>, design: string, acc: string[], memo: Map<string, string[]> = new Map()): string[] {
-    if(design.length === 0) return acc;
-    if(memo.has(design)) return memo.get(design) as string[];
-    for(const pattern of patterns) {
-        if(design.startsWith(pattern)) {
-            const newDesign = design.slice(pattern.length);
-            const newAcc = acc.concat(pattern);
-            const result = combinations(patterns, newDesign, newAcc, memo);
-            if(result.length > 0) {
-                memo.set(design, result);
-                return result;
-            }
-        }
-    }
-    memo.set(design, []);
-    return [];
-}
-
 function partOne(input: string[]): number {
     const { patterns, designs } = parseInput(input);
+    let max: number = Array.from(patterns.keys()).reduce((acc, p) => {
+        return Math.max(acc, p.length);
+    }, 0);
     return designs.reduce((acc, design) => {
-        return acc + (combinations(patterns, design, []).length > 0 ? 1 : 0);
+        return acc + (arrangements(patterns, design, max, new Map<string, number>()) > 0 ? 1 : 0);
     }, 0);
 }
 
@@ -106,7 +67,6 @@ function partTwo(input: string[]): number {
 }
 
 test(day, () => {
-    debug(`${day} ${new Date()}\n\n`, day, false);
     expect(partOne(getExampleInput(day))).toBe(6);
     expect(partOne(getDayInput(day))).toBe(226);
 
