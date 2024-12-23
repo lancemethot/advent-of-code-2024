023406fb10408b4af4e54d24cb2528498a080177
lancemethot <132783723+lancemethot@users.noreply.github.com>
2024-12-22 19:29:58 -0500

    day 22 part 2

--- day22/day22.test.ts
+++ day22/day22.test.ts
@@ -1,6 +1,16 @@
-import { debug, getDayInput, getExampleInput } from '../utils';
+import { getDayInput, getExampleInput } from '../utils';
 
 const day = 'day22';
+
+type Buyer = {
+    generations: Generation[];
+}
+
+type Generation = {
+    secret: bigint;
+    price: number;
+    change: number;
+}
 
 function mix(secret: bigint, value: bigint): bigint {
     return secret ^ value;
@@ -25,6 +35,38 @@ function stepTo(secret: bigint, steps: number): bigint {
     return acc;
 }
 
+function price(secret: bigint): number {
+    let str: string = String(secret);
+    return Number.parseInt(str.charAt(str.length - 1));
+}
+
+function next(secret: bigint): Generation {
+    let currentPrice: number = price(secret);
+    let nextSecret: bigint = stepTo(secret, 1);
+    let nextPrice: number = price(nextSecret);
+    return {
+        secret: nextSecret,
+        price: nextPrice,
+        change: nextPrice - currentPrice
+    };
+}
+
+function getBuyerPatterns(buyer: Buyer, memo: Map<string, number[]>): void {
+    const patterns: Set<string> = new Set();
+    for(let i = 5; i < buyer.generations.length; i++) {
+        let key: string = buyer.generations.slice(i - 4, i).map(gen => gen.change).join(',');
+
+        if(patterns.has(key)) continue;
+        patterns.add(key);
+
+        if(memo.has(key)) {
+            memo.get(key)?.push(buyer.generations[i-1].price);
+        } else {
+            memo.set(key, [ buyer.generations[i-1].price ]);
+        }
+    }
+}
+
 function partOne(input: string[]): bigint {
     const buyers: bigint[] = input.map(line => BigInt(line));
     return buyers.reduce((acc, buyer) => {
@@ -32,9 +74,32 @@ function partOne(input: string[]): bigint {
     }, 0n);
 }
 
-test(day, () => {
-    debug(`[**${day}**] ${new Date()}`, day, false);
+function partTwo(input: string[]): number {
 
+    const buyers: Buyer[] = input.map(line => {
+        let secret: bigint = BigInt(line);
+        let p: number = price(secret);
+        let c: number = 0;
+        return {
+            generations: [ { secret, price: p, change: c }]
+        } as Buyer;
+    });
+
+    const memo: Map<string, number[]> = new Map();
+    buyers.forEach(buyer => {
+        for(let i = 0; i <= 2000; i++) {
+            buyer.generations.push(next(buyer.generations[i].secret));
+        }
+        getBuyerPatterns(buyer, memo);
+    });
+
+    return Array.from(memo.values()).reduce((acc, sequence) => {
+        return Math.max(acc, sequence.reduce((acc, val) => acc + val, 0));
+    }, 0);
+
+}
+
+test(day, () => {
     expect(mix(42n, 15n)).toBe(37n);
     expect(prune(100000000n)).toBe(16113920n);
 
@@ -51,4 +116,7 @@ test(day, () => {
 
     expect(partOne(getExampleInput(day))).toBe(37327623n);
     expect(partOne(getDayInput(day))).toBe(19458130434n);
+
+    expect(partTwo(getExampleInput(day, 2))).toBe(23);
+    expect(partTwo(getDayInput(day))).toBe(2130);
 });
